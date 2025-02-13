49f836c4da0fc8d88173a6da357a6e978793e175
knuggets <arya.sahu22b@iiitg.ac.in>
2025-02-13 10:28:01 +0530

    Feb13

---
+++ CS331-SWELab/Modules/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='header-contents'>
+                <h2>Order your style here</h2>
+                <p>Choose from a diverse fashion closet. Our mission is to style you the way you want anytime and anywhere!</p>
+                <button>View my options</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
